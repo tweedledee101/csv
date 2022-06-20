b17dfb10599c31d36f4d9ce5095aa3ba8821a5f9
fatimesino <68020860+fatimesino@users.noreply.github.com>
2022-06-20 11:36:23 -0500

    Create csv_dictreader_mem.py

---
+++ csv_dictreader_mem.py
@@ -0,0 +1,88 @@
+
+import csv
+import json
+import math
+import os
+from pathlib import Path
+from pprint import pprint
+
+
+
+###################################################################################################
+#  Environment Variables
+###################################################################################################
+maximum_allowable_rows = 499999
+file_counter = 0
+row_counter = 0
+source_dir = Path('/User/fatime/dev/csv/csvp2/')
+output_dir = Path('/User/fatime/dev/csv/csvp2/output_files_mem/')
+files = source_dir.glob('membership.csv')
+fieldname = []
+
+print(f"Making output directory now")
+make_output_file_directory = f'mkdir {output_dir}'
+os.system(make_output_file_directory)
+
+
+
+##################################################################################################
+# Processing Checks
+##################################################################################################
+for file in files:
+    print(file, "\n")
+
+with open('membership.csv', 'r') as f:
+    dict_object = csv.DictReader(f)
+    for row in dict_object:
+        fieldnames = row.keys()
+        print(fieldnames)
+        for field in fieldnames:
+            fieldname.append(field)
+        break
+
+    #print(f"This is our list fieldname:\n{fieldname}")
+
+    # nothing too crazy here, I just wanna check programmatically how much data we'll be processing
+    reader = csv.reader(f,delimiter = ",")
+    data = list(reader)
+    row_count = len(data)
+    print(f"Total number of rows: {row_count}")
+
+    #Using math ceiling to round up to the nearest whole number
+    files_to_create = math.ceil(row_count/maximum_allowable_rows)
+    #print(f"We're going to create {files_to_create} files")
+
+    f.close()
+
+
+
+
+###################################################################################################
+# File Creation
+###################################################################################################
+
+with open('membership.csv', 'r') as f:
+    dict_object = csv.DictReader(f)
+
+    #Now, while this read file is open, we need to create an output file.
+    while file_counter < files_to_create:
+        output_filename = f'membership_{file_counter}.csv'
+        print(f"File Number: {file_counter}")
+        print(f"Filename: {output_filename}\n")
+        file_counter += 1
+
+        # The top of our loop states that we can only continue as long as the file_counter is less than the number
+        # of files we would like to create: Now, we need to open a new file to write to
+        with open(f'{output_dir}/{output_filename}', 'w', newline="", encoding="utf-8") as output:
+            writer = csv.DictWriter(output, fieldnames=fieldname)
+            writer.writeheader()
+
+            for row in dict_object:
+                row_counter += 1
+                #print(f"inside For Loop, File Number {file_counter}")
+                #print(f"Inside For Loop Row Number{row_counter}")
+                #print(f"Row Values: {row}")
+                writer.writerow(row)
+                if row_counter >= maximum_allowable_rows:
+                    row_counter = 0
+                    break
